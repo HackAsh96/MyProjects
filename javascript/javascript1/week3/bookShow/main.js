d2456b07631beb79a3f7f568bf91bd88f9cbd956
Th3P3dr0 <th3p3dr096@gmail.com>
2018-10-13 07:51:21 +0200

    adding style and images from JS

--- javascript/javascript1/week3/bookShow/main.js
+++ javascript/javascript1/week3/bookShow/main.js
@@ -5,69 +5,90 @@ let booksDetails =[{
   id: myArray[0],
   title: "The Love",
   language: "english",
-  author:"Paul"
+  author:"Paul",
+  imgsrc: 'https://i.harperapps.com/covers/9780060959470/y648.jpg'
 },
 {
   id: myArray[1],
   title: "Desert",
   language: "italian",
-  author:"Marco"
+  author:"Marco",
+  imgsrc: 'https://images-na.ssl-images-amazon.com/images/I/51I8u-BR-bL._SX302_BO1,204,203,200_.jpg'
 },
 {
   id: myArray[2],
   title:"Disgrace",
   language: "chinese",
-  author:"Mao"
+  author:"Mao",
+  imgsrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/JMCoetzee_Disgrace.jpg/200px-JMCoetzee_Disgrace.jpg'
 },
 {
   id: myArray[3],
   title:"Geek",
   language: "spanish",
-  author:"Pablo"
+  author:"Pablo",
+  imgsrc: 'https://images.gr-assets.com/books/1366699063l/13872.jpg'
 },
 {
   id: myArray[4],
   title:"Gilead",
   language: "french",
-  author:"Margout"
+  author:"Margout",
+  imgsrc: 'https://images-na.ssl-images-amazon.com/images/I/91k3cIBsWSL.jpg'
 },
 {
   id: myArray[5],
   title:"Jest",
   language: "german",
-  author:"Muller"
+  author:"Muller",
+  imgsrc: 'https://images-na.ssl-images-amazon.com/images/I/81KBldResmL.jpg'
 },
 {
   id: myArray[6],
   title:"Lolita",
   language:"spanish",
-  author:"Rosa"
+  author:"Rosa",
+  imgsrc: 'http://litreactor.com/sites/default/files/images/column/2015/01/lolita.png'
 },
 {
   id: myArray[7],
   title: "Maus",
   language: "polish",
-  author: "Lewandoswi"
+  author: "Lewandoswi",
+  imgsrc: 'https://images-na.ssl-images-amazon.com/images/I/51JW5sREyEL._SX362_BO1,204,203,200_.jpg'
 },
 {
   id: myArray[8],
-  title:"Never Let Me Go",
+  title:"NLM Go",
   language:"arabic",
-  author:"Mohammed"
+  author:"Mohammed",
+  imgsrc: 'https://images.gr-assets.com/books/1353048590l/6334.jpg'
 },
 {
   id: myArray[9],
   title:"Poem",
   language: "danish",
-  author: "Sorensen"
+  author: "Sorensen",
+  imgsrc: 'http://bookcoverarchive.com/wp-content/uploads/amazon/good_poems_for_hard_times.jpg'
 }
 ]
 function books() {
   const ul = document.createElement('ul');
   for(i=0;i<myArray.length;i++){
     const li = document.createElement('li');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const p1 = document.createElement('p');
+    const img = document.createElement('img');
+    p.innerHTML = booksDetails[i].language;
+    p1.innerHTML = booksDetails[i].author;
+    h2.innerHTML = booksDetails[i].title;
+    img.setAttribute('src',booksDetails[i].imgsrc);
+    li.appendChild(h2);
+    li.appendChild(p);
+    li.appendChild(p1);
+    li.appendChild(img);
     ul.appendChild(li);
-    li.innerHTML=li.innerHTML + myArray[i];
   }
   document.getElementById('myBooks').appendChild(ul);
 }
